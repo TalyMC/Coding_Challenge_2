cee5366130088c0142ed0e54579021830922725c
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-01-26 21:42:34 -0500

    Task 1 - Store Inventory.

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+//Task 1: Store Inventory
+let products = ["Canvas","Paint Brush","Pencils", "Acrylic Paint", "Fineliner Pens"]
+products.push("Watercolor paint");
+products.pop();
+
+console.log("Updated product listing", products);
+
+//Task 2: Student Scores
