8ae96a1cdd177942b42f1997ca559711fe87ef10
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-01-26 22:28:13 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -9,11 +9,12 @@ console.log("Updated product listing", products);
 let scores = [55, 62, 90, 75, 89]
 scores[1] = 96
 
-let total = scores.reduce((sum, score) => sum + score, 0);
-let averageScores = total/scores.length
+let averageScores = scores.reduce((sum, score) => sum + score, 0)/ scores.length;
 
 console.log("Updated Second Score", scores);
-console.log("Total Scores", total);
 console.log("Average of Scores", averageScores);
 
 //Task 3: Employee Records
+let employee = {
+
+}
